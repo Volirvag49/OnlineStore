14f18f869e63b9765f6173f1185bc33222d82968
KirillGavrilov <kirill.gavrilov@testit.ru>
2019-11-28 10:24:06 +0300

    error  interceptor

--- Store/Store.ApiStore/ClientApp/src/app/core/core.module.ts
+++ Store/Store.ApiStore/ClientApp/src/app/core/core.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HttpTokenInterceptor } from './interceptors/http.token.interceptor';
+import { HttpErrorInterceptor } from './interceptors/http.error.interceptor';
+
 import {
     ApiService,
     ProductService
@@ -14,6 +16,7 @@ import {
     ],
     providers: [
         { provide: HTTP_INTERCEPTORS, useClass: HttpTokenInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true },
         ApiService,
         ProductService
     ],
